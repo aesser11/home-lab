e31a421ac4fea95e9042bb276b61bafcb626320f
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-04-29 18:27:44 -0500

    Update backup_shares.sh

--- unraid/scripts/backup_shares.sh
+++ unraid/scripts/backup_shares.sh
@@ -4,12 +4,12 @@ logdate=$(date +%F)
 scriptdir=/boot/config/plugins/user.scripts/scripts/backup_shares
 
 # Do the backups
-rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads
-rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/media /mnt/disks/easystore8tb/backups/media
-rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/share /mnt/disks/easystore8tb/backups/share
-rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/store /mnt/disks/easystore8tb/backups/store
-rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine
+rsync -avhiP --stats --log-file=$scriptdir/downloads-$logdate.log /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads
+#rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/media /mnt/disks/easystore8tb/backups/media
+rsync -avhiP --stats --log-file=$scriptdir/backups-$logdate.log /mnt/user/share /mnt/disks/easystore8tb/backups/share
+rsync -avhiP --stats --log-file=$scriptdir/store-$logdate.log /mnt/user/store /mnt/disks/easystore8tb/backups/store
+rsync -avhiP --stats --log-file=$scriptdir/timemachine-$logdate.log /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine
 
 # Send notification with result
 # -m "Result $result"
-/usr/local/emhttp/webGui/scripts/notify -e "Unraid Server Notice" -s "Unassigned Devices" -d "Backup script finished" -i "normal"
+/usr/local/emhttp/webGui/scripts/notify -e "Unraid Backup Notice" -s "Backup Script Status" -d "Backup script finished" -i "normal"
