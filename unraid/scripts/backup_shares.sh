4af216d6adeefea657402c976d41caea81248d27
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-04-30 09:57:18 -0500

    add unmount, improved logging and notifications

--- unraid/scripts/backup_shares.sh
+++ unraid/scripts/backup_shares.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # Set variables
 logdate=$(date +%F)
-scriptdir=/boot/config/plugins/user.scripts/scripts/backup_shares
+logdir=/boot/config/plugins/user.scripts/scripts/backup_shares/logs
+error_output="No errors for this run"
 
 # Do the backups
-rsync -avhiP --stats --log-file=$scriptdir/downloads-$logdate.log /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads
-#rsync -avhiP --stats --log-file=$scriptdir/$logdate.log /mnt/user/media /mnt/disks/easystore8tb/backups/media
-rsync -avhiP --stats --log-file=$scriptdir/backups-$logdate.log /mnt/user/share /mnt/disks/easystore8tb/backups/share
-rsync -avhiP --stats --log-file=$scriptdir/store-$logdate.log /mnt/user/store /mnt/disks/easystore8tb/backups/store
-rsync -avhiP --stats --log-file=$scriptdir/timemachine-$logdate.log /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine
+rsync -avhi --stats --log-file=$logdir/downloads-$logdate.log /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads 2> "$error_output"
+#rsync -avhi --stats --log-file=$scriptdir/$logdate.log /mnt/user/media /mnt/disks/easystore8tb/backups/media 2> "$error_output"
+rsync -avhi --stats --log-file=$logdir/backups-$logdate.log /mnt/user/share /mnt/disks/easystore8tb/backups/share 2> "$error_output"
+rsync -avhi --stats --log-file=$logdir/store-$logdate.log /mnt/user/store /mnt/disks/easystore8tb/backups/store 2> "$error_output"
+rsync -avhi --stats --log-file=$logdir/timemachine-$logdate.log /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine 2> "$error_output"
 
 # Send notification with result
-# -m "Result $result"
-/usr/local/emhttp/webGui/scripts/notify -e "Unraid Backup Notice" -s "Backup Script Status" -d "Backup script finished" -i "normal"
+/usr/local/emhttp/webGui/scripts/notify -e "Unraid Backup Notice" -s "Backup Script Status" -d "Backup script finished" -i "normal" -m "Error output: $error_output"
+
+# Unmount drive -- does this unmount any drive in this slot or will the external always be considered sdl1? -- does this spin down the drive as well?
+/sbin/umount '/dev/sdl1' 2>&1
