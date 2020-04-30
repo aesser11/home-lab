4c57b5ab7f3300f61cd249caa7021b9b7ca3f732
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-04-30 11:52:54 -0500

    fixed some oopsies

--- unraid/scripts/backup_shares.sh
+++ unraid/scripts/backup_shares.sh
@@ -4,11 +4,11 @@ error_output="No errors for this run"
 summary_output="No summary for this run"
 
 # Do the backups -- fix stderr and stdout commands
-rsync -ah --stats 2> "$error_output" 1> "$summary_output"
-rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
-rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
-rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
-#rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
+rsync -ah --stats /mnt/user/store /mnt/disks/easystore8tb/backups/store 2> "$error_output" 1> "$summary_output"
+rsync -ah --stats /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine 2>> "$error_output" 1>> "$summary_output"
+rsync -ah --stats /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads 2>> "$error_output" 1>> "$summary_output"
+rsync -ah --stats /mnt/user/share /mnt/disks/easystore8tb/backups/share 2>> "$error_output" 1>> "$summary_output"
+#rsync -ah --stats /mnt/user/media /mnt/disks/easystore8tb/backups/media 2>> "$error_output" 1>> "$summary_output"
 
 # Unmount drive
 /sbin/umount '/dev/sdl1' 2>> "$error_output" 1>> "$summary_output"
