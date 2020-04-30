75bc89d415345be3677e74b89818b133a8f31fe6
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-04-30 12:36:53 -0500

    Update backup_shares.sh

--- unraid/scripts/backup_shares.sh
+++ unraid/scripts/backup_shares.sh
@@ -2,15 +2,15 @@
 # Set variables
 log_output="No stderr or stdout for this run"
 
-# Do the backups
-rsync -ah --stats /mnt/user/store /mnt/disks/easystore8tb/backups/store 2>&1 > "$log_output"
-rsync -ah --stats /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine 2>&1 >> "$log_output"
-rsync -ah --stats /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads 2>&1 >> "$log_output"
-rsync -ah --stats /mnt/user/share /mnt/disks/easystore8tb/backups/share 2>&1 >> "$log_output"
-#rsync -ah --stats /mnt/user/media /mnt/disks/easystore8tb/backups/media 2>&1 >> "$log_output"
+# Do the backups -- send output to the console as well as capturing it
+log_output=$((rsync -ah --stats /mnt/user/store /mnt/disks/easystore8tb/backups/store)2>&1)
+log_output=$((rsync -ah --stats /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine)2>&1)
+log_output=$((rsync -ah --stats /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads)2>&1)
+log_output=$((rsync -ah --stats /mnt/user/share /mnt/disks/easystore8tb/backups/share)2>&1)
+#log_output=$((rsync -ah --stats /mnt/user/media /mnt/disks/easystore8tb/backups/media)2>&1)
 
 # Unmount drive
-/sbin/umount '/dev/sdl1' 2>&1 >> "$log_output"
+log_output=$((/sbin/umount '/dev/sdl1')2>&1)
 
 # Send notification with result
 /usr/local/emhttp/webGui/scripts/notify -e "Shares Backup" -s "Shares Backup Script Status" -d "backup_shares.sh finished running" -i "normal" -m "Log Output: $log_output"
