07d734397afdae2ee41362ac092a8fe86e84df64
aesser11 <9578875+aesser11@users.noreply.github.com>
2020-04-30 11:51:05 -0500

    simplified output

--- unraid/scripts/backup_shares.sh
+++ unraid/scripts/backup_shares.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Set variables
-logdate=$(date +%F)
-logdir=/boot/config/plugins/user.scripts/scripts/backup_shares/logs
 error_output="No errors for this run"
 summary_output="No summary for this run"
 
-# Do the backups -- output summary to summary_output
-rsync -ah --stats --log-file=$logdir/store-$logdate.log /mnt/user/store /mnt/disks/easystore8tb/backups/store 2> "$error_output" 1> "$summary_output"
-rsync -ah --stats --log-file=$logdir/timemachine-$logdate.log /mnt/user/timemachine /mnt/disks/easystore8tb/backups/timemachine 2>> "$error_output" 1>> "$summary_output"
-rsync -ah --stats --log-file=$logdir/downloads-$logdate.log /mnt/user/downloads /mnt/disks/easystore8tb/backups/downloads 2>> "$error_output" 1>> "$summary_output"
-rsync -ah --stats --log-file=$logdir/share-$logdate.log /mnt/user/share /mnt/disks/easystore8tb/backups/share 2>> "$error_output" 1>> "$summary_output"
+# Do the backups -- fix stderr and stdout commands
+rsync -ah --stats 2> "$error_output" 1> "$summary_output"
+rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
+rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
+rsync -ah --stats 2>> "$error_output" 1>> "$summary_output"
 #rsync -ah --stats --log-file=$scriptdir/$logdate.log /mnt/user/media /mnt/disks/easystore8tb/backups/media 2>> "$error_output" 1>> "$summary_output"
 
-# Unmount drive -- does this unmount any drive in this slot or will the external always be considered sdl1? -- does this spin down the drive as well?
+# Unmount drive
 /sbin/umount '/dev/sdl1' 2>> "$error_output" 1>> "$summary_output"
 
 # Send notification with result
