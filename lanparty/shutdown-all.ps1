f1941bf15aab857d1b01b9889b3310bf8df2b06f
aesser11 <9578875+aesser11@users.noreply.github.com>
2025-09-02 00:20:22 -0500

    update with lots of notes

---
+++ lanparty/shutdown-all.ps1
@@ -0,0 +1,4 @@
+#shutdown-all.ps1
+ssh -o "StrictHostKeyChecking no" "sleepo@192.168.1.217" -- 'shutdown /p /f'
+ssh -o "StrictHostKeyChecking no" "peepo@192.168.1.216" -- 'shutdown /p /f'
+Pause
--- lanparty/update-machines.ps1
+++ lanparty/update-machines.ps1
@@ -1,6 +1,18 @@
 #update-machines.ps1
 #https://powershell.one/code/11.html
 
+#Get input prompt for which machines to turn on
+#TODO create a MAP of HOSTS sleepo
+$MacAddresses = @(
+  "00:d8:61:f7:41:40", #sleepo@192.168.1.217
+  "e8:9c:25:78:f6:9b"  #peepo@192.168.1.216 
+)
+
+$AddressesForSSH = @(
+  "sleepo@192.168.1.217",
+  "peepo@192.168.1.216" 
+)
+
 function Invoke-WakeOnLan
 {
   param
@@ -68,3 +80,34 @@ function Invoke-WakeOnLan
     $UDPclient.Dispose()
   }
 }
+
+foreach ($macAddress in $macAddresses) {
+  Invoke-WakeOnLan -MacAddress $macAddress
+}
+
+Pause
+
+#$ip = Get-NetNeighbor | Where-Object { $_.LinkLayerAddress -eq "00:d8:61:f7:41:40" } | Select-Object IPAddress
+#write-host $ip.IPAddress[1]
+
+#How to open applications in the foregroujnd?
+#- create a scheduled task!
+
+#Open each of Steam, Discord, Nvidia app, etc. and let them download any updates.
+
+#"C:\Program Files (x86)\Steam\steam.exe"
+#"C:\Program Files (x86)\Steam\steam.exe" -shutdown
+#delete steam installation folder -> /config/loginusers.vdf????
+#delete steam installation folder -> /userdata???
+
+#taskkill /F /IM discord.exe /T
+#RMDIR /S /Q "C:\Users\%USERNAME%\AppData\Roaming\Discord\Local Storage\leveldb"
+#"C:\Users\%username%\AppData\Local\Discord\Update.exe --processStart Discord.exe"
+
+#"C:\Program Files\NVIDIA Corporation\NVIDIA app\CEF\NVIDIA App.exe"
+#Start-Process ".\[nvidia-driver-name].exe" -ArgumentList "/s"
+
+#foreach ($Address in $AddressesForSSH) {
+#  ssh $Address | UsoClient.exe StartScan
+#https://www.urtech.ca/2018/11/solved-easily-script-windows-10-to-download-install-and-restart-for-windows-updates/
+#}
