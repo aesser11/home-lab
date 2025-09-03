dc5ee5e3f5f2e20fac082e0f1830c900f834b339
aesser11 <9578875+aesser11@users.noreply.github.com>
2025-09-02 20:42:52 -0500

    refactor and rename

--- lanparty/shutdown-all.ps1
+++ lanparty/shutdown-all.ps1
@@ -2,3 +2,4 @@
 ssh -o "StrictHostKeyChecking no" "sleepo@192.168.1.217" -- 'shutdown /p /f'
 ssh -o "StrictHostKeyChecking no" "peepo@192.168.1.216" -- 'shutdown /p /f'
 Pause
+## TODO set up public key openssh generation and extraction
--- lanparty/wake-on-lan.ps1
+++ lanparty/wake-on-lan.ps1
@@ -86,28 +86,3 @@ foreach ($macAddress in $macAddresses) {
 }
 
 Pause
-
-#$ip = Get-NetNeighbor | Where-Object { $_.LinkLayerAddress -eq "00:d8:61:f7:41:40" } | Select-Object IPAddress
-#write-host $ip.IPAddress[1]
-
-#How to open applications in the foregroujnd?
-#- create a scheduled task!
-
-#Open each of Steam, Discord, Nvidia app, etc. and let them download any updates.
-
-#"C:\Program Files (x86)\Steam\steam.exe"
-#"C:\Program Files (x86)\Steam\steam.exe" -shutdown
-#delete steam installation folder -> /config/loginusers.vdf????
-#delete steam installation folder -> /userdata???
-
-#taskkill /F /IM discord.exe /T
-#RMDIR /S /Q "C:\Users\%USERNAME%\AppData\Roaming\Discord\Local Storage\leveldb"
-#"C:\Users\%username%\AppData\Local\Discord\Update.exe --processStart Discord.exe"
-
-#"C:\Program Files\NVIDIA Corporation\NVIDIA app\CEF\NVIDIA App.exe"
-#Start-Process ".\[nvidia-driver-name].exe" -ArgumentList "/s"
-
-#foreach ($Address in $AddressesForSSH) {
-#  ssh $Address | UsoClient.exe StartScan
-#https://www.urtech.ca/2018/11/solved-easily-script-windows-10-to-download-install-and-restart-for-windows-updates/
-#}
