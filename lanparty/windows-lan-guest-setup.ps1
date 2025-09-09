3b8662eec7397a2c4c9776f797fd3dfb55956b7c
aesser11 <9578875+aesser11@users.noreply.github.com>
2025-09-08 21:42:17 -0500

    Update windows-lan-guest-setup.ps1

--- lanparty/windows-lan-guest-setup.ps1
+++ lanparty/windows-lan-guest-setup.ps1
@@ -3,3 +3,45 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File $PSCommandPath" -Verb RunAs
     exit
 }
+
+
+#################
+# Reimage Steps #
+#################
+$firstRunFunctions1 = @(
+    # user input required
+    "renameComputer",#change-prompt-logic
+    
+    # tailored to my desired settings
+    "downloadSoftware"
+)
+
+$finalFirstRunFunctions3 = @(
+    # automated and universal
+    "removeWin10Apps",
+
+    "remainingStepsToText"
+)
+
+$everyRunFunctions2 = @(
+    # universal functions
+    "disableTelemetry",
+    "setTimeZone",
+    "disableStickyKeys",
+    "soundCommsAttenuation",
+    "disableMouseAcceleration",
+
+    # tailored to my desired settings
+    "modifyWindowsFeatures",
+    "setPowerProfile",
+
+    # functions exclusively for myself
+    "enableGuestSMBShares",
+    "uninstallOneDrive",
+    "disableLocalIntranetFileWarnings",
+    "advancedExplorerSettings"
+)
+
+$finalEveryRunFunctions4 = @(
+    "promptForRestart"
+)
